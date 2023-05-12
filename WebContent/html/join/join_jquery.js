a7c78a6195a815162457a8bc12012c404727b5d4
jaehyeonkim0 <akfakfhkd@naver.com>
2023-05-12 12:25:30 +0900

    commit_cmj

--- WebContent/html/join/join_jquery.js
+++ WebContent/html/join/join_jquery.js
@@ -7,8 +7,20 @@ $(document).ready(function(){
 			alert("아이디는 필수 입력 항목입니다");
 			$("input[name='id'").focus();
 			return false;
+		}else if(!idCheck2($("input[name='id']").val())) {
+			alert("아이디는 영문 또는 영문+숫자로 작성해주세요");
+			$("input[name='id'").focus();
+			return false;
+		}else if(!idCheck3($("input[name='id']").val())) {
+			alert("아이디는 5~10글자로 작성해주세요");
+			$("input[name='id'").focus();
+			return false;
 		}else if($("input[name='pwd'").val()==""){
 			alert("비밀번호는 필수 입력 항목입니다");
+			$("input[name='pwd'").focus();
+			return false;
+		}else if(!pwdCheck($("input[name='pwd']").val())) {
+			alert("비밀번호는 영문,숫자,특수문자 1글자 이상 조합하여 작성해주세요");
 			$("input[name='pwd'").focus();
 			return false;
 		}else if($("input[name='pwd-check']").val()=="") {
@@ -23,9 +35,21 @@ $(document).ready(function(){
 			alert("이름은 필수 입력 항목입니다");
 			$("input[name='name']").focus();
 			return false;
+		}else if(!nameCheck($("input[name='name']").val())) {
+			alert("이름은 한글로만 작성해주세요");
+			$("input[name='name']").focus();
+			return false;
 		}else if($("#input-email").val()==""){
 			alert("이메일은 필수 입력 항목입니다");
 			$("input[name='email1']").focus();
+			return false;
+		}else if(!emailCheck($("#input-email").val())){
+			alert("이메일은 영문+숫자로 입력해주세요");
+			$("input[name='email1']").focus();
+			return false;
+		}else if($("input[name='email2']").val()=="" || $("#selectbox-email").val()=="default"){
+			alert("이메일 주소를 전부 작성해주세요");
+			$("input[name='email2']").focus();
 			return false;
 		}else if($("#selectbox-mobile").val()=="default") {
 			alert("통신사를 선택해주세요");
@@ -40,7 +64,7 @@ $(document).ready(function(){
 			$("input[name='phone2']").focus();
 			return false;
 		}else if(!phoneCheck($("input[name='phone2']").val())) {
-			alert("휴대전화는 3,4자리로 입력해주세요");
+			alert("휴대전화는 숫자 3,4자리로 입력해주세요");
 			$("input[name='phone2']").focus();
 			return false;
 		}else if($("input[name='phone3']").val()=="") {
@@ -48,10 +72,10 @@ $(document).ready(function(){
 			$("input[name='phone3']").focus();
 			return false;
 		}else if(!phoneCheck($("input[name='phone3']").val())) {
-			alert("휴대전화는 3,4자리로 입력해주세요");
+			alert("휴대전화는 숫자 3,4자리로 입력해주세요");
 			$("input[name='phone3']").focus();
 			return false;
-		}else if($("#agreement:checked").length!=3) {
+		}else if($("#checkbox-agreement:checked").length!=3) {
 			alert("약간 동의를 체크해주세요");
 			$("#agreement").focus();
 			return false;
@@ -59,23 +83,33 @@ $(document).ready(function(){
 			joinForm.submit();
 			location.href="../main/index.html";
 		}
-			
-		
 	});
 /*************************************************************************
 						input null값 확인
 *************************************************************************/
-	function idCheck(asValue){
-		var regex = /[^0-9]/g;
-		
-		return regex.test(asValue);
-	}
-	
-	function idCheck1(asValue) {
-		var regex1 = /[a-zA-Z]{5,10}/g;
+	/*function idCheck1(asValue){
+		var regex1 = /[^0-9]/g;
 		
 		return regex1.test(asValue);
+	}*/
+	
+	function idCheck2(asValue) {
+		var regex2 = /^[a-zA-Z0-9]*$/;
+		
+		return regex2.test(asValue);
 	}
+	
+	function idCheck3(asValue) {
+		var regex3 = /[a-zA-Z0-9]{5,10}/g;
+		
+		return regex3.test(asValue);
+	}
+	
+	/*function idCheck4(asValue){//영어로 시작하고 숫자로 시작하면 안됨
+		var regex4 = /^[A-Za-z]{1}[A-Za-z0-9]{3,19}$/;
+		
+		return regex4.test(asValue);
+	}*/
 	
 	function pwdCheck(asValue) {
 		var regex = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$/g;
@@ -85,6 +119,12 @@ $(document).ready(function(){
 	
 	function nameCheck(asValue) {
 		var regex =  /^[가-힣]{2,4}$/;
+		
+		return regex.test(asValue);
+	}
+	
+	function emailCheck(asValue){
+		var regex = /^[A-za-z0-9]{4,12}$/;
 		
 		return regex.test(asValue);
 	}
@@ -100,10 +140,10 @@ $(document).ready(function(){
 		if($("input[name='id']").val()==""){
 			$("#idMsg").text("필수항목입니다").css("color","red").css("font-size","11px").css("display","block");
 		}else {
-			if(!idCheck($("input[name='id']").val())) {
-				$("#idMsg").text("영문부터 5글자 이상으로 작성해주세요").css("color","red").css("font-size","11px").css("display","block");
-			}else if(!idCheck1($("input[name='id']").val())){
-				$("#idMsg").text("5글자 이상으로 작성해주세요").css("color","red").css("font-size","11px").css("display","block");
+			if(!idCheck2($("input[name='id']").val())){
+				$("#idMsg").text("영문 또는 숫자로 작성해주세요").css("color","red").css("font-size","11px").css("display","block");
+			}else if(!idCheck3($("input[name='id']").val())) {
+				$("#idMsg").text("5~10자리로 작성해주세요").css("color","red").css("font-size","11px").css("display","block");
 			}else {
 				$("#idMsg").css("display","none");
 			}
@@ -158,7 +198,13 @@ $(document).ready(function(){
 		if($("input[name='email1']").val()==""){
 			$("#emailMsg").text("필수항목입니다").css("color","red").css("font-size","11px").css("display","block");
 		}else{
-			$("#emailMsg").css("display","none");
+			
+			if(!emailCheck($("input[name='email1']").val())){
+				$("#emailMsg").text("이메일은 영문+숫자로 입력해주세요").css("color","red").css("font-size","11px").css("display","block");
+			}else{
+				$("#emailMsg").css("display","none");
+			}
+			
 		}
 	});
 	
@@ -177,10 +223,11 @@ $(document).ready(function(){
 	$("#selectbox-email").change(function(){
 		
 		if($("#selectbox-email").val()=="default") {
+			$("input[name='email2']").val("");
 			$("#emailMsg").text("이메일을 선택해주세요").css("color","red").css("font-size","11px").css("display","block");
 			$("#selectbox-email").focus();
 			return false;
-		}else if($("#selectbox-email").val()=="self") {
+		}else if($("#selectbox-email").val()=="direct") {
 			$("input[name='email2']").val("").focus();
 		}else {
 			$("input[name='email2']").val($("#selectbox-email").val());
@@ -200,10 +247,16 @@ $(document).ready(function(){
 	});
 /*************************************************************************
 					이용약관 & 개인정보 수집 팝업창
-*************************************************************************/
+*************************************************************************
 	$("#agreement-link").click(function(){
 		
-		window.open("http://localhost:9000/cmjgames/agreement.html","이용약관","width=600,height=400,scrollbars=yes");
+		$("#modal.modal-overlay").css("display","flex");
+		
+	});
+	
+	$("#close-area").click(function(){
+		
+		$("#modal.modal-overlay").css("display","none");
 		
 	});
 	
@@ -247,8 +300,26 @@ $(document).ready(function(){
 			$("input[name='chk-agree']").prop("checked",false);
 	});
 /*************************************************************************
-	
+							모달창
 *************************************************************************/
+/** Agreement **/
+// Agreement
+$("a").click(function(){
+    $("#modal").show();
+    
+    if($(this).attr("id") == "detail1"){
+    	$("#agreement-content").html($("div.detail1").html());
+    }
+    else if($(this).attr("id") == "detail2"){
+    	$("#agreement-content").html($("div.detail2").html());
+    }
+});
+	
+	
+// Agreement-btn
+$("button[name='btn-agreement']").click(function() {
+	$("#modal").hide();
+});
 /*************************************************************************
 	
 *************************************************************************/
