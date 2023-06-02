592ace15361497848422ea135996c1811e929caf
JeonB <newjeonb@gmail.com>
2023-06-02 17:14:32 +0900

    Delete uniquegames directory
    ㅇㅇ

--- uniquegames/src/main/java/com/uniquegames/model/SessionConstants.java
+++
@@ -1,6 +0,0 @@
-package com.uniquegames.model;
-
-public interface SessionConstants {
-    String LOGIN_MEMBER = "loginMember";
-    //먼저 HttpSession에서 로그인용으로 사용할 세션 id는 여기저기서 사용될 것이기 때문에 상수로 뺀다.
-}
--- uniquegames/src/main/java/com/uniquegames/vo/CartVo.java
+++
@@ -1,5 +0,0 @@
-package com.uniquegames.vo;
-
-public class CartVo {
-	
-}
