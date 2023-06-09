320085ae10932ac68d6ddc9802b289b24d47b647
kimbs5899 <krsder@naver.com>
2023-06-09 13:44:49 +0900

    index kbs

---
+++ src/main/java/com/uniquegames/repository/IndexMapper.java
@@ -0,0 +1,54 @@
+package com.uniquegames.repository;
+
+import com.uniquegames.vo.GameVo;
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface IndexMapper {
+
+    @Select("SELECT G.ID, G.NAME, G.IMAGE_PATH, G.GAME_GENRE, G.DONATION_STATUS, G.DESCRIPTION, L.LIKE_COUNT FROM GAME G INNER JOIN (SELECT ID, LENGTH(LIKELIST) - LENGTH(REPLACE(LIKELIST, ',', '')) AS LIKE_COUNT FROM GAME) L ON G.ID = L.ID")
+    List<GameVo> getGameList();
+
+    @Select("SELECT * FROM GAME WHERE ID=?#{id}")
+    GameVo getGame(GameVo vo);
+
+    @Select("SELECT * FROM GAME WHERE DONATION_STATUS = 1")
+    List<GameVo> getDonationList();
+
+    @Select("SELECT G.ID, G.NAME, G.IMAGE_PATH, G.GAME_GENRE, G.DONATION_STATUS, G.DESCRIPTION, L.LIKE_COUNT FROM GAME G INNER JOIN (SELECT ID, LENGTH(LIKELIST) - LENGTH(REPLACE(LIKELIST, ',', '')) + 1 AS LIKE_COUNT FROM GAME) L ON G.ID = L.ID ORDER BY LIKE_COUNT DESC")
+    List<GameVo> getRankingList();
+
+    @Insert("INSERT INTO GAME(NAME,IMAGE_PATH,GAME_GENRE,DONATION_STATUS,DESCRIPTION,LIKELIST) VALUES(#{name},#{image_path},#{game_genre},#{donation_status},#{description},0)")
+    void insertGame(GameVo vo);
+
+    @Update("UPDATE GAME SET NAME=#{name} WHERE ID=#{id}")
+    void updateGame(GameVo vo);
+
+    @Delete("DELETE GAME WHERE ID=#{id}")
+    void deleteGame(GameVo vo);
+
+
+
+    @Select("SELECT COUNT(*) FROM GAME WHERE CONCAT(',', LIKELIST, ',') LIKE '%,#{gid},%' AND ID = #{mid}")
+    int hasLiked(int gid, int mid);
+
+    @Update("UPDATE GAME SET LIKELIST = CONCAT(IFNULL(LIKELIST, ''), ',#{mid}') WHERE ID = #{gid}")
+    void addLikeInfo(int gid, int mid);
+
+    @Delete("SET @gameId = #{gid}" +
+            "SET @targetMid = #{mid}" +
+            "UPDATE GAME SET LIKELIST = (CASE WHEN FIND_IN_SET(@targetMid, LIKELIST) > 0 THEN TRIM(BOTH ',' FROM REPLACE(CONCAT(',', LIKELIST, ','), CONCAT(',', @targetMid, ','), ','))" +
+            "WHEN FIND_IN_SET(@targetMid, LIKELIST) = 1 THEN TRIM(BOTH ',' FROM REPLACE(CONCAT(@targetMid, ',', LIKELIST), CONCAT(@targetMid, ','), ''))" +
+            "WHEN FIND_IN_SET(@targetMid, LIKELIST) = LENGTH(LIKELIST) - LENGTH(REPLACE(LIKELIST, ',', '')) + 1 THEN TRIM(BOTH ',' FROM REPLACE(CONCAT(LIKELIST, ',', @targetMid), CONCAT(',', @targetMid), ''))" +
+            "ELSE LIKELIST END)WHERE ID = @gameId")
+    void removeLikeInfo(int gid, int mid);
+
+
+
+}
---
+++ src/main/java/com/uniquegames/service/IndexServiceMapper.java
@@ -0,0 +1,56 @@
+package com.uniquegames.service;
+
+import com.uniquegames.repository.IndexMapper;
+import com.uniquegames.vo.GameVo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class IndexServiceMapper {
+
+    @Autowired
+    IndexMapper indexMapper;
+
+    public List<GameVo> getGameList() {
+        return indexMapper.getGameList();
+    }
+
+    public GameVo getGame(GameVo vo) {
+        return indexMapper.getGame(vo);
+    }
+
+    public List<GameVo> getDonationList() {
+        return indexMapper.getDonationList();
+    }
+
+    public List<GameVo> getRankingList() {
+        return indexMapper.getRankingList();
+    }
+
+    public void insertGame(GameVo vo) {
+        indexMapper.insertGame(vo);
+    }
+
+    public void updateGame(GameVo vo) {
+        indexMapper.updateGame(vo);
+    }
+
+    public void deleteGame(GameVo vo) {
+        indexMapper.deleteGame(vo);
+    }
+
+    public int hasLiked(int gid, int mid) {
+        return indexMapper.hasLiked(gid, mid);
+    }
+
+    public void addLikeInfo(int gid, int mid) {
+        indexMapper.addLikeInfo(gid, mid);
+    }
+
+    public void removeLikeInfo(int gid, int mid) {
+        indexMapper.removeLikeInfo(gid, mid);
+    }
+
+}
