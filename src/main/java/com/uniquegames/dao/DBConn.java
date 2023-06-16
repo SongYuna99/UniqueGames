446792db7e42444e743dfbd0902d44f6cd4fae48
Corendo6 <durrl5897@gmail.com>
2023-06-16 14:55:09 +0900

    개인 환경 변경사항 커밋 롤백

--- src/main/java/com/uniquegames/dao/DBConn.java
+++ src/main/java/com/uniquegames/dao/DBConn.java
@@ -9,7 +9,7 @@ public class DBConn {
 	// Field
 	String driver = "com.mysql.cj.jdbc.Driver";
 	String url = "jdbc:mysql://localhost:3306/uniquegames";
-	String user = "unique_games";
+	String user = "uniquegames";
 	String pass = "1234";
 
 	Connection conn;
